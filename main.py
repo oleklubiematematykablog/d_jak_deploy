ceccb5eafef432fbd33ee90e2b5e7a104737e4af
oleklubiematematykablog <62251653+oleklubiematematykablog@users.noreply.github.com>
2020-04-05 18:43:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,23 +3,48 @@ from fastapi import Request
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
-class Patient(BaseModel): #patient request
+class PatientRq(BaseModel): #patient request
 	name: str
-	surname: str
-	patientId: int = 0
+	surename: str
+
+class PatientResp(BaseModel): #patient response
+	patientId: int
+	patient: PatientRq
 
 app = FastAPI()
+
+#1
 
 @app.get("/")
 def root():
 	return {"message": "Hello World during the coronavirus pandemic!"}
 
-app.id = -1
+#2
 
+@app.get("/method")
+def response():
+	return {"method": "GET"}
+
+@app.post("/method")
+def response():
+	return {"method": "POST"}
+
+@app.put("/method")
+def response():
+	return {"method": "PUT"}
+
+@app.delete("/method")
+def response():
+	return {"method": "DELETE"}
+
+#3
+
+app.id = -1
+app.patients = []
 
 
 @app.post("/patient")
-async def receive_something(patient: Patient):
+def receive_patient(patient: PatientRq):
 	app.id += 1
-	patient.patientId = app.id
-	return {"id": patient.id, "patient": {"name": patient.name, "surename": patient.surname}}
+	app.patients.append(patient)
+	return app.patients
